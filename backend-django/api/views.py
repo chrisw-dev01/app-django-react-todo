3c5dd49b5a0937107784dfe8b75a00fc7ccc6053
Chris W <69566919+chrisw-dev01@users.noreply.github.com>
2022-06-29 19:50:01 +0100

    feat(api): implement signup, login using token authentication

--- backend-django/api/views.py
+++ backend-django/api/views.py
@@ -1,6 +1,13 @@
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 class TodoListCreate(generics.ListCreateAPIView):
   serializer_class = TodoSerializer
@@ -14,6 +21,7 @@ class TodoListCreate(generics.ListCreateAPIView):
   def perform_create(self, serializer):
     serializer.save(user=self.request.user)
 
+
 class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
   serializer_class = TodoSerializer
   permission_classes = [permissions.IsAuthenticated]
@@ -22,6 +30,7 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     user = self.request.user
     # User can only update, delete own posts
     return Todo.objects.filter(user=user)
+
 
 class TodoToggleComplete(generics.UpdateAPIView):
   serializer_class = TodoToggleCompleteSerializer
@@ -34,3 +43,47 @@ class TodoToggleComplete(generics.UpdateAPIView):
   def perform_update(self, serializer):
     serializer.instance.completed=not(serializer.instance.completed)
     serializer.save()
+
+
+@csrf_exempt
+def signup(request):
+  if request.method == 'POST':
+    try:
+      data = JSONParser().parse(request)
+      user = User.objects.create_user(username=data['username'])
+      password = data['password']
+      user.save()
+
+      token = Token.objects.create(user=user)
+
+      return JsonResponse(
+        {'token': str(token)}, status=201)
+    
+    except IntegrityError:
+      return JsonResponse(
+        {'error': 'Username taken. Choose another username'},
+        status=400)
+
+@csrf_exempt
+def login(request):
+  if request.method == 'POST':
+    data = JSONParser().parse(request)
+    user = authenticate(
+      request,
+      username = data['username'],
+      password = data['password'])
+    if user is None:
+      return JsonResponse(
+        {'error': 'Unable to login. Please check username and password.'},
+        status = 400)
+    else:
+      # Return user token
+      try:
+        token = Token.objects.get(user=user)
+      except:
+        # If token not in db, create new one
+        token = Token.objects.create(user=user)
+      return JsonResponse(
+        {'token': str(token)}, status=201)
+
+
--- backend-django/backend/settings.py
+++ backend-django/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -101,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 
 # Internationalization
