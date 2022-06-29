7ddfffeff9024e90052cafd0cbe21aa35c3c23c6
Chris W <69566919+chrisw-dev01@users.noreply.github.com>
2022-06-29 20:48:27 +0100

    feat(frontend): implement mui icons

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -5,6 +5,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 
 import './index.css';
 
