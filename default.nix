478dd50f81d4ace631207a3e1dcb7be46c78d7cd
Chris W <69566919+chrisw-dev01@users.noreply.github.com>
2022-06-29 11:50:50 +0100

    Initialize repository

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python = pkgs.python3;
+  python-with-my-packages = my-python.withPackages (p: with p; [
+    django
+    djangorestframework
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    python-with-my-packages
+    pkgs.nodejs_latest
+  ];
+  shellHook = ''
+    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+    # maybe set more env-vars
+  '';
+}
