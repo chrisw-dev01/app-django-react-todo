2a14908f1f5cddf4eaa67e88e6045d6c532561af
Chris W <69566919+chrisw-dev01@users.noreply.github.com>
2022-06-29 21:29:11 +0100

    feat(frontend): implement navbar

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,7 +1,21 @@
 import * as React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import StarIcon from '@mui/icons-material/StarBorder';
+import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+
+// import { Switch, Route, Link } from 'react-router-dom';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -14,13 +28,46 @@ import TodosList from './components/todos-list';
 import Login from './components/login';
 import Signup from './components/signup';
 
+let user = "Henry";
+
 function App() {
   return (
-    <Box sx={{ width: '100%', maxWidth: 500 }}>
-      <Typography variant="h1" component="div" gutterBottom>
-        Hello World
-      </Typography>
-    </Box>
+    <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            Todos App
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/todos"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Todos
+            </Link>
+          </nav>
+          { user ? (
+            <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+              Logout ({user})
+            </Button>
+          ) : (
+            <>
+              <Button href="/login" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+                Login
+              </Button>
+              <Button href="/signup" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+                Signup
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
   );
 }
 
